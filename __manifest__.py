4bab0208aca41a059db414cc9270c41ab1aa8822
M.Saeb <48291783+M-Saeb@users.noreply.github.com>
2021-05-23 16:40:03 +0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,26 +1,20 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "iw_accounting",
+    'name': "IW Accounting",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': """Customizing Accounting for IW made by Integerated Path""",
 
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Integerated Path",
+    'website': "https://www.int-path.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Accounting',
+    'version': '1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'account_accountant],
 
     # always loaded
     'data': [
